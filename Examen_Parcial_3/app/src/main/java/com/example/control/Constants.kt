d02a2d0a7e6c3af6ee766409be26501aba32a46e
Luis-Elfego-Velazquez-Garcia <2030911@upt.edu.mx>
2023-08-06 20:29:29 -0600

    Proyecto_06

---
+++ Examen_Parcial_3/app/src/main/java/com/example/control/Constants.kt
@@ -0,0 +1,7 @@
+// Constants.kt
+package com.example.control
+
+object Constants {
+    const val REQUEST_ENABLE_BT = 1
+}
+
---
+++ Examen_Parcial_3/app/src/main/java/com/example/control/MainActivity2.java
@@ -0,0 +1,53 @@
+package com.example.control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ImageButton boton_luz,boton_musica,boton_ventanas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ///Abrir Otras Interfaces///
+        boton_luz=(ImageButton) findViewById(R.id.boton_luz);
+        boton_luz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear el Intent para abrir la nueva actividad
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                // Iniciar la nueva actividad
+                startActivity(intent);
+            }
+        });
+        boton_musica=(ImageButton) findViewById(R.id.boton_musica);
+        boton_musica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear el Intent para abrir la nueva actividad
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                // Iniciar la nueva actividad
+                startActivity(intent);
+            }
+        });
+
+        /*boton_ventanas=(ImageButton) findViewById(R.id.boton_ventanas);
+        boton_ventanas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear el Intent para abrir la nueva actividad
+                Intent intent = new Intent(getApplicationContext(), ventanas.class);
+                // Iniciar la nueva actividad
+                startActivity(intent);
+            }
+        });*/
+
+    }
+}
